a56f38b3cf13cc26090f175da54adb7f463a2143
myannyax <myannyax@gmail.com>
2019-05-17 13:55:33 +0300

    Task2 and Task1 fixes

--- task10/src/Basics.hs
+++ task10/src/Basics.hs
@@ -17,37 +17,41 @@ tail' (_:xs) = xs
 
 -- 3. take' возвращает первые n >= 0 элементов исходного списка
 take' :: Int -> [a] -> [a]
-take' n (x:xs) = x:(take' (n - 1) xs)
 take' 0 _ = []
+take' n (x:xs) = x:(take' (n - 1) xs)
 
 -- 4. drop' возвращает список без первых n >= 0 элементов; если n больше длины
 -- списка, то пустой список.
 drop' :: Int -> [a] -> [a]
-drop' n (_:xs) = (drop' (n - 1) xs)
 drop' 0 a = a
+drop' n (_:xs) = (drop' (n - 1) xs)
 
 -- 5. filter' возвращает список из элементов, для которых f возвращает True
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f (x:xs) | ((f x) == True) = x : (filter' f xs)
-filter' f (_:xs) = (filter' f xs)
 filter' _ [] = []
+filter' f (x:xs) = if (f x)
+                   then x : (filter' f xs)
+                   else (filter' f xs)
 
 -- 6. foldl'' последовательно применяет функцию f к элементу списка l и значению,
 -- полученному на предыдущем шаге, начальное значение
 -- foldl'' (+) 0 [1, 2, 3] == (((0 + 1) + 2) + 3)
 -- foldl'' (*) 4 [] == 4
 foldl'' :: (a -> b -> a) -> a -> [b] -> a
-foldl'' f z (x:xs) = foldl'' f (f z x) xs
 foldl'' _ z [] = z
+foldl'' f z (x:xs) = foldl'' f (f z x) xs
+
 
 -- 7. concat' принимает на вход два списка и возвращает их конкатенацию
 -- concat' [1,2] [3] == [1,2,3]
 concat' :: [a] -> [a] -> [a]
-concat' (a:as) b = a : concat' as b
 concat' [] b = b
+concat' (a:as) b = a : concat' as b
+
 
 -- 8. quickSort' возвращает его отсортированный список
 -- quickSort' должен быть реализован через алгоритм QuickSort
 -- (выбор pivot может быть любым)
 quickSort' :: Ord a => [a] -> [a]
-quickSort' a = concat' (quickSort' (filter' (< head' a) a)) (concat' (quickSort' (filter' (== head' a) a)) (quickSort' (filter' (> head' a) a)))
+quickSort' [] = []
+quickSort' a = concat' (quickSort' (filter' (< head' a) a)) (concat' (filter' (== head' a) a) (quickSort' (filter' (> head' a) a)))
--- task10/src/Robots.hs
+++ task10/src/Robots.hs
@@ -35,13 +35,13 @@ getHealth (_, _, myHealth) = myHealth
 -- состояние робота
 
 setName :: Name -> Robot -> Robot
-setName = undefined
+setName newName (_, myAttack, myHealth) = (newName, myAttack, myHealth)
 
 setAttack :: Attack -> Robot -> Robot
-setAttack = undefined
+setAttack newAttack (myName, _, myHealth) = (myName, newAttack, myHealth)
 
 setHealth :: Health -> Robot -> Robot
-setHealth = undefined
+setHealth newHealth (myName, myAttack, _) = (myName, myAttack, newHealth)
 
 -- Шаг 2.
 -- Напишите функцию, которая ведет себя как __str__
@@ -51,7 +51,7 @@ setHealth = undefined
 -- > "Marvin, attack: 100, health: 500"
 
 printRobot :: Robot -> String
-printRobot = undefined
+printRobot (myName, myAttack, myHealth) = myName ++ ", attack: " ++ (show myAttack) ++ ", health: " ++ (show myHealth)
 
 -- Давайте теперь научим роботов драться друг с другом
 -- Напишем функцию damage которая причиняет роботу урон
@@ -65,7 +65,7 @@ damage victim amount = let
 -- Вам понадобится вспомогательная функция isAlive, которая бы проверяла, жив робот или не очень
 -- Робот считается живым, если его уровень здоровья строго больше нуля.
 isAlive :: Robot -> Bool
-isAlive = undefined
+isAlive (_, _, myHealth) = myHealth > 0
 
 -- Затем, используя функцию damage, напишите функцию, которая моделирует один раунд схватки между
 -- двумя роботами
@@ -76,7 +76,8 @@ isAlive = undefined
 -- Обратите внимание, что неживой робот не может атаковать. В этом случае нужно просто
 -- вернуть второго робота, как будто ничего и не было
 fight :: Robot -> Robot -> Robot
-fight attacker defender = undefined
+fight attacker defender | isAlive attacker = damage defender (getAttack attacker)
+fight _ defender = defender
 
 -- Наконец, напишите функцию, которая бы моделировала три раунда схватки между
 -- двумя роботами и возвращала бы победителя. Схватка происходит следующим образом:
@@ -88,24 +89,33 @@ fight attacker defender = undefined
 -- Если же так вышло, что после трех раундов у обоих роботов одинаковый уровень жизни, то
 -- победителем считается тот, кто ударял первым(то есть атакующий робот)
 threeRoundFight :: Robot -> Robot -> Robot
-threeRoundFight attacker defender = undefined
+threeRoundFight attacker defender = let
+        defender_after_first_round = fight attacker defender
+        attacker_after_second_round = fight defender_after_first_round attacker
+        defender_after_third_round = fight attacker_after_second_round defender_after_first_round
+     in
+         if ((getHealth defender_after_third_round) > (getHealth attacker_after_second_round))
+         then defender_after_third_round
+         else attacker_after_second_round
+
+
 
 -- Шаг 4.
 -- Создайте список из трех роботов(Абсолютно любых, но лучше живых, мы собираемся их побить)
 roboter :: [Robot]
-roboter = undefined
+roboter = [(robot "First" 15 43), (robot "Second" 12 37), (robot "Third" 17 39)]
 
 -- Затем создайте четвертого
 neueRobot :: Robot
-neueRobot = undefined
+neueRobot = (robot "NewRobot" 19 100)
 
 -- Используя частичное применение напишите функцию, которая бы принимала на вход робота
 -- и атаковала бы его роботом neueRobot
 neueRobotAttak :: Robot -> Robot
-neueRobotAttak = undefined
+neueRobotAttak = fight neueRobot
 
 -- Наконец, используя filter определите, кто из роботов, которых вы положили в список roboter,
 -- выживет, если neueRobot сразится с ним в одном раунде (neueRobot атакует).
 -- survivors должен быть подсписком roboter.
 survivors :: [Robot]
-survivors = undefined
+survivors = filter (isAlive . neueRobotAttak) roboter
